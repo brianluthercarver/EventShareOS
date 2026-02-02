cdb94e8d4a6ac4392f32789a5791072931c7f466
brian carver <brianluthercarver@gmail.com>
2026-02-02 07:58:19 -0500

    removing not needed build and testing files

--- test/binary_dir/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/brian/code/c_code/EventShareOS/EventShareOS
-# Build directory: /home/brian/code/c_code/EventShareOS/test/binary_dir
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
