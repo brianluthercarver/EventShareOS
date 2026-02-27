85b9f159e4f3730cbe83bbb47c7af0f7445785b1
brian carver <brian.carver@beaconmedaes.com>
2026-02-27 08:15:21 -0500

    removed library file and CTest gen file

--- test/Unity/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/brian/code/c_code/EventShareOS/test/Unity
-# Build directory: /home/brian/code/c_code/EventShareOS/test/Unity
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
